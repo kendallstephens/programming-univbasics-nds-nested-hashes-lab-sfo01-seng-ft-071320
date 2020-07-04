26eae1f2245979a135e00467dba6b2ce4237ebc9
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-04 20:34:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,6 +32,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  new_value = "Joelle van Dyne (PGOAT)"
-  literal_aoh[1][:name] = new_value
+   aoh[row][key] = new_value
+   aoh_update
 end
